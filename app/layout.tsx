fe0ba8333f0be48bf8eed979ebaf3e3d3c5b95eb
luis-obsidian <luotasss@gmail.com>
2025-02-19 13:27:46 -0300

    updating post md to html parser and adding gh css md styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import Link from "next/link";
 import { Linkedin, Github, Home} from 'lucide-react';
 const geistSans = Geist({
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -17,10 +17,18 @@ export default async function PostPage({ params }: Props) {
     const postData = await getPostData(id);
 
     return (
-        <div>
-            <h1>{postData.title}</h1>
-            <p>{postData.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml || '' }} />
-        </div>
+        <main className="max-w-3xl mx-auto p-6">
+            <div className="mb-6 text-center">
+                <h1 className="text-4xl font-bold mb-2 capitalize">{postData.title}</h1>
+                <p className="text-gray-500 dark:text-gray-400 text-sm">
+                Published on {new Date(postData.date).toLocaleDateString("pt-BR")}
+                </p>
+            </div>
+
+            <article
+                className="markdown-body p-8"
+                dangerouslySetInnerHTML={{ __html: postData.contentHtml || "" }}
+            />
+        </main>
     );
 }
