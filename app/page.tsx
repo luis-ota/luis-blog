750588f3596c648f0404bb324a77a392d9d18809
luis <luotasss@gmail.com>
2025-02-19 23:42:47 -0300

    edinting some styles

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
                                 href={`posts/${id}`} 
                                 className="flex flex-col p-4 rounded-lg hover:bg-violet-400 w-full md:w-2/5 gap-4 bg-gray-700">
                                     <div className="flex flex-col md:flex-row gap-4 items-center">
-                                        {img && <Image className="w-full md:w-36 h-24 object-cover rounded-lg" src={ img } alt={title} width={80} height={70}/>}
+                                        {img && <Image className="w-full md:max-w-36 h-32 md:h-[6.5rem] object-cover rounded-lg" src={ img } alt={title} width={80} height={60}/>}
                                         <div>
                                             <h2 className="text-xl font-semibold"> { title } </h2>
                                             <p className="text-gray-400"> { description } </p>
